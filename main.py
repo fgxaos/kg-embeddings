e751a2b68260b25443df6c7f6eab5159425fd0d5
François <francois.garderes@student.ecp.fr>
2021-04-11 22:42:54 +0200

    Uses configuration file for hyperparameters

--- main.py
+++ main.py
@@ -1,104 +1,14 @@
 ### LIBRARIES ###
 # Global libraries
-import os
-import wandb
-
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
+import yaml
 
 # Custom libraries
-from data.datasets.dataloader import KGDataModule
-
-from models.TransE.TransE import TransE
-from models.DistMult.DistMult import DistMult
-from models.ComplEx.ComplEx import ComplEx
-from models.RotatE.RotatE import RotatE
-from models.RotatE.pRotate import pRotatE
-from models.ConvE.ConvE import ConvE
-
-### VARIABLES ###
-n_epochs = 10
-batch_size = 128
-negative_sample_size = 128
+from run_exp import run_experiment
 
 ### MAIN CODE ###
-def run_experiment(dataset: str, model_name: str, use_wandb: bool):
-    """Runs a single experiment.
+# Import the global configuration
+with open("cfg.yml", "r") as yml_file:
+    cfg = yaml.safe_load(yml_file)
 
-    Args:
-        dataset: str
-            name of the dataset to use
-        model_name: str
-            name of the model to use
-        use_wandb: bool
-            whether to use wandb
-    """
-    if use_wandb:
-        wandb.init(project="mlns_kg_embedding")
-        wandb.config.dataset = dataset
-        wandb.config.model_name = model_name
-        wandb.config.n_epochs = n_epochs
-        wandb.config.batch_size = batch_size
-        wandb.config.negative_sample_size = negative_sample_size
-
-    # Load the dataset
-    dataset_path = os.path.join("data", "datasets", dataset)
-    data_module = KGDataModule(
-        dataset_path,
-        num_workers=4,
-        batch_size=batch_size,
-        negative_sample_size=negative_sample_size,
-    )
-
-    # Create a model instance
-    n_entity = len(data_module.entity2id)
-    n_relation = len(data_module.relation2id)
-    if model_name == "TransE":
-        model = TransE(n_entity, n_relation, 256, 12.0, 1e-3, use_wandb)
-    elif model_name == "DistMult":
-        model = DistMult(n_entity, n_relation, 256, 12.0, 1e-3)
-    elif model_name == "ComplEx":
-        model = ComplEx(n_entity, n_relation, 256, 12.0, 1e-3)
-    elif model_name == "RotatE":
-        model = RotatE(n_entity, n_relation, 256, 12.0, 1e-3)
-    elif model_name == "pRotatE":
-        model = pRotatE(n_entity, n_relation, 256, 12.0, 1e-3)
-    elif model_name == "ConvE":
-        model = ConvE(n_entity, n_relation, 20, 10, 32, 3, 0.2, 0.2, 0.3, 0.0, 1e-3)
-    else:
-        raise ValueError("Wrong model name given")
-
-    # Load the pretrained model
-    ckpt_name = f"{model_name}-{dataset}-{n_epochs}"
-    ckpt_file = os.path.join("checkpoints", f"{ckpt_name}.ckpt")
-    if not os.path.exists(ckpt_file):
-        # Train the model
-        checkpoint_callback = ModelCheckpoint(
-            monitor="val_score",
-            dirpath="checkpoints",
-            filename=ckpt_name,
-            save_top_k=1,
-            mode="min",
-        )
-        trainer = pl.Trainer(max_epochs=n_epochs, callbacks=[checkpoint_callback])
-        trainer.fit(model, datamodule=data_module)
-
-    # Test the pretrained model
-    if model_name == "TransE":
-        model_test = TransE.load_from_checkpoint(ckpt_file)
-    elif model_name == "DistMult":
-        model_test = DistMult.load_from_checkpoint(ckpt_file)
-    elif model_name == "ComplEx":
-        model_test = ComplEx.load_from_checkpoint(ckpt_file)
-    elif model_name == "RotatE":
-        model_test = RotatE.load_from_checkpoint(ckpt_file)
-    elif model_name == "pRotatE":
-        model_test = pRotatE.load_from_checkpoint(ckpt_file)
-    else:
-        raise ValueError("Wrong model name given")
-
-    trainer = pl.Trainer()
-    trainer.test(model_test, datamodule=data_module)
-
-
-run_experiment("FB15k-237", "TransE")
+# For each list parameter, run different experiments
+run_experiment(cfg)
---
+++ run_exp.py
@@ -0,0 +1,64 @@
+### LIBRARIES ###
+# Global libraries
+import os
+import wandb
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+# Custom libraries
+from models.TransE.run_model import run_transe
+from models.DistMult.run_model import run_distmult
+from models.ComplEx.run_model import run_complex
+from models.RotatE.run_model import run_rotate
+from models.pRotatE.run_model import run_protate
+from models.ConvE.run_model import run_conve
+
+### FUNCTION DEFINITION ###
+def run_experiment(cfg):
+    """Runs a single experiment.
+
+    Args:
+        cfg: Dict
+            configuration to use
+    """
+    if cfg["use_wandb"]:
+        wandb.init(project="mlns_kg_embedding")
+        wandb.config.dataset = cfg["dataset"]
+        wandb.config.model_name = cfg["model_name"]
+        wandb.config.n_epochs = cfg["n_epochs"]
+
+        wandb.config.batch_size = cfg["batch_size"]
+        wandb.config.negative_sample_size = cfg["negative_sample_size"]
+
+    # Load the dataset
+    dataset_path = os.path.join("data", "datasets", cfg["dataset"])
+
+    # Load the pretrained model
+    ckpt_name = f"{cfg['model_name']}-{cfg['dataset']}-{cfg['n_epochs']}"
+    ckpt_file = os.path.join("checkpoints", f"{ckpt_name}.ckpt")
+
+    if not os.path.exists(ckpt_file):
+        checkpoint_callback = ModelCheckpoint(
+            monitor="val_score",
+            dirpath="checkpoints",
+            filename=ckpt_name,
+            save_top_k=1,
+            mode="min",
+        )
+    else:
+        checkpoint_callback = None
+
+    if cfg["model_name"] == "TransE":
+        run_transe(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    elif cfg["model_name"] == "DistMult":
+        run_distmult(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    elif cfg["model_name"] == "ComplEx":
+        run_complex(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    elif cfg["model_name"] == "RotatE":
+        run_rotate(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    elif cfg["model_name"] == "pRotatE":
+        run_protate(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    elif cfg["model_name"] == "ConvE":
+        run_conve(dataset_path, ckpt_file, checkpoint_callback, cfg)
+    else:
+        raise ValueError("Wrong model name given")
